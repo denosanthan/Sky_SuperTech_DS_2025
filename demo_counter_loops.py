44c22c1028dc28ed7ee12137a554c62f77095e05
Deenesh <deenesh.santheeswaran@sky.uk>
2025-09-30 11:03:04 +0100

    Created demo_counter_loops.py

---
+++ demo_counter_loops.py
@@ -0,0 +1,10 @@
+count = 0 # 1. Initialise counter
+while count < 10: # 2.Test condition
+    print(count)
+    count += 1
+
+for num in range(0, 10, 1):
+    print(num)
+
+for num in range(10):
+    print(num)
