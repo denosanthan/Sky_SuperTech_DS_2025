ded2a73197acda5d8ba74679be91cbbdcfaedc74
Deenesh <deenesh.santheeswaran@sky.uk>
2025-10-01 16:45:02 +0100

    created tank class, data and methods in tank.py

---
+++ Sky_GOT/app/tank.py
@@ -0,0 +1,42 @@
+class Tank:
+    # Class has 2 components = Attributes/Data + Behaviour/Methods
+    def __init__(self, country, model):
+        self.country = country
+        self.model = model
+        self.speed = 0
+        self.health = 100
+        self.location = {'x':0, 'y':0, 'z': 0}
+        self.direction = 0
+        self.shells = 20
+        # No EXPLICIT RETURN as called implicitly.
+
+
+def accel(self, increase):
+    self._speed += increase
+    return None
+
+
+def decel(self, decrease):
+    self._speed -= decrease
+    return None
+
+
+def rotate_left(self, degrees):
+    self._direction -= degrees % 360
+    return None
+
+
+def rotate_right(self, degrees):
+    self._direction += degrees % 360
+    return None
+
+
+def shoot(self):
+    self._shells -= 1
+    return None
+
+
+def take_damage(self, damage):
+    self._health -= damage
+    return None
+
