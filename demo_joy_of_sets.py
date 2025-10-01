ef704df4ced68953316773579cc21b287e60c8c6
Deenesh <deenesh.santheeswaran@sky.uk>
2025-10-01 13:59:41 +0100

    Created demo_joy_of_sets.py

---
+++ demo_joy_of_sets.py
@@ -0,0 +1,13 @@
+from pprint import saferepr
+
+marvel_fans = {'ayo'}, 'charles', 'arash', 'zubin', 'diren','sage', 'donald'}
+dc_fans = set()
+
+dc_fans.add('donald')
+dc_fans.add('ben')
+dc_fans.add('deenesh')
+
+dc_fans.pop()
+print(f"Fans of Marvel= {marvel_fans}")
+print(f"Fans of DC Fans= {dc_fans}")
+
